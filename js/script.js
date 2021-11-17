3a6d50db189eae9d94da925aba78cb85f107a927
Vyacheslav <70888287+drimstor@users.noreply.github.com>
2021-11-17 11:16:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
 let photos = [
-	'img/photo1.jpg',
-	'img/photo2.jpg',
-	'img/photo3.jpg',
-	'img/photo4.jpg'
+	'../img/photo1.jpg',
+	'../img/photo2.jpg',
+	'../img/photo3.jpg',
+	'../img/photo4.jpg'
   ];
   
   
